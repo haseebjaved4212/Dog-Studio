3cdb6dcded09188925afc6d0024184ff04c75f0f
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-01-05 09:09:15 +0500

    feat: Add initial Dog component with 3D canvas setup and a dog model file.

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,8 +1,10 @@
 import { Canvas } from "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei"
 
 const Dog = () => {
     return (
         <Canvas>
+            <OrbitControls />
             <mesh>
                 <boxGeometry args={[1, 1, 1]} />
                 <meshBasicMaterial color={0x00ff00} />
