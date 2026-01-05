080fedf8442540140f13d3b3d80fae1ec51d292b
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-01-05 08:46:54 +0500

    Fix An Error

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,9 +1,14 @@
-import {Canvas } from "@react-three/fiber"
+import { Canvas } from "@react-three/fiber"
 
 const Dog = () => {
-  return (
-    <Canvas></Canvas>
-  )
+    return (
+        <Canvas>
+            <mesh>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshBasicMaterial color={0x00ff00} />
+            </mesh>
+        </Canvas>
+    )
 }
 
 export default Dog
