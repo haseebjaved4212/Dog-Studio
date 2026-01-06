7d2feba9f5076357d5fcd0c0a89b010814d4305b
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-01-06 12:15:51 +0500

    feat: add 3D dog component with model loading, textures, and animation.

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -58,7 +58,7 @@ const Dog = () => {
         <>
             <primitive object={model.scene} position={[0.25, -0.55, 0]} rotation={[0, Math.PI / 3.9, 0]} />
             <directionalLight position={[0, 5, 5]} color={0xffffff} intensity={10} />
-            <OrbitControls />
+            {/* <OrbitControls /> */}
         </>
     )
 }
