89a676d72e4f792cf933b84d94746f3ab8dbb3dd
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-01-12 08:32:11 +0500

    Complete Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,19 @@ import "../App.css"
 const Footer = () => {
     return (
         <>
-
-            <div className="footer" id="footer">
+<div className="footer" id="footer">
+            <div className="left" >
                 <h1>DogStudio</h1>
                 <p>Copyright © 2026 DogStudio. All rights reserved.</p>
-
-
-            </div>
-
+  </div>
+<div className="right">
+       <i className="ri-facebook-line"></i>
+        <i className="ri-twitter-line"></i>
+        <i className="ri-instagram-line"></i>
+        <i className="ri-linkedin-line"></i>
+</div>
+          
+</div>
 
         </>
     )
